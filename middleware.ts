f7c38bfab09a7e3f2450fc6af38452d626c0703e
Saifee Rahman Saif <saifeer1019@gmail.com>
2025-03-15 09:53:07 +0600

    clerk

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,8 @@ import { clerkMiddleware } from '@clerk/nextjs/server'
 export default clerkMiddleware()
 
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*),  '/upcoming',
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*), 
+    '/upcoming',
   '/meeting(.*)',
   '/previous',
   '/recordings',
